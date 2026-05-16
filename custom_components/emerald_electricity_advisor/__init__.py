6efd2fd947c2c92dd73a1fb9c76065527f7dd809
Michael Power <51349539+Michael-O-Power@users.noreply.github.com>
2026-05-16 16:26:23 +1000

    Initial version

--- custom_components/emerald_electricity_advisor/__init__.py
+++ custom_components/emerald_electricity_advisor/__init__.py
@@ -1,1 +1,41 @@
+"""The Emerald Electricity Advisor integration."""
+import asyncio
+import logging
+from typing import Final
 
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import Platform
+from homeassistant.core import HomeAssistant
+from homeassistant.exceptions import ConfigEntryNotReady
+
+from .coordinator import EmeraldDataUpdateCoordinator
+
+_LOGGER = logging.getLogger(__name__)
+
+PLATFORMS: Final = [Platform.SENSOR]
+
+
+async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Set up Emerald Electricity Advisor from a config entry."""
+    hass.data.setdefault("emerald_electricity_advisor", {})
+
+    coordinator = EmeraldDataUpdateCoordinator(hass, entry)
+
+    await coordinator.async_config_entry_first_refresh()
+
+    if not coordinator.last_update_success:
+        raise ConfigEntryNotReady
+
+    hass.data["emerald_electricity_advisor"][entry.entry_id] = coordinator
+
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
+
+    return True
+
+
+async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Unload a config entry."""
+    if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
+        hass.data["emerald_electricity_advisor"].pop(entry.entry_id)
+
+    return unload_ok
